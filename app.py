f96bc63b04130d3af33947f1d3fd7c845107eb9f
Dholakiya <sdholak@calstatela.edu>
2024-04-05 00:49:01 -0700

    Implemented Create and Get Nodes endpoints

--- app.py
+++ app.py
@@ -1,14 +1,31 @@
 # Imports the Flask class.
 # Creates an instance of the Flask class. 
 # This instance is our WSGI application.
-from flask import Flask
+from flask import Flask, jsonify, request
 
 app = Flask(__name__)
 
-@app.route('/')
-def hello_world():
-    return 'Hello, Distributed Systems World!'
+# In-memory data structure to store nodes
+nodes = []
 
-# Runs the app in debug mode, which is helpful during development as it will reload the app for you upon changes.
+@app.route('/nodes', methods=['POST'])
+def create_node():
+    data = request.json
+    new_node = {
+        'id': len(nodes) + 1,
+        'status': data.get('status', 'active'),
+        'load': data.get('load', 0)
+    }
+    nodes.append(new_node)
+    return jsonify(new_node), 201
+
+@app.route('/nodes', methods=['GET'])
+def get_nodes():
+    return jsonify(nodes)
+
 if __name__ == '__main__':
     app.run(debug=True)
+
+# Runs the app in debug mode, which is helpful during development as it will reload the app for you upon changes.
+# if __name__ == '__main__':
+#     app.run(debug=True)
