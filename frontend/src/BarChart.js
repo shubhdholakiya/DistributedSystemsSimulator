5629ab9410542e76e62b5ca679733e6d23e33e47
Shubh Dholakiya <53867459+shubhdholakiya@users.noreply.github.com>
2025-03-23 19:59:06 -0700

    Update BarChart.js

--- frontend/src/BarChart.js
+++ frontend/src/BarChart.js
@@ -2,19 +2,19 @@ import React from 'react';
 import { Bar } from 'react-chartjs-2';
 import 'chart.js/auto';
 
-const BarChart = () => {
+const BarChart = ({ labels, dataPoints }) => {
   const data = {
-    labels: ['Label 1', 'Label 2'],
-  datasets: [{
-    label: 'Dataset 1',
-    data: [value1, value2],
-    backgroundColor: ['rgba(255, 99, 132, 0.2)', 'rgba(54, 162, 235, 0.2)'],
-    borderColor: ['rgba(255,99,132,1)', 'rgba(54, 162, 235, 1)'],
-    borderWidth: 1
+    labels: labels,
+    datasets: [{
+      label: 'Nodes Status',
+      data: dataPoints,
+      backgroundColor: ['rgba(255, 99, 132, 0.2)', 'rgba(54, 162, 235, 0.2)'],
+      borderColor: ['rgba(255,99,132,1)', 'rgba(54, 162, 235, 1)'],
+      borderWidth: 1
     }]
   };
 
-  return <Bar data={data} key={Date.now()} />;
+  return <Bar data={data} />;
 };
 
 export default BarChart;
